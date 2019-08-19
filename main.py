88e2a5bd88df030446e3c83b345fdf5376706976
Alexey Kuklin <AlexeyKuklin@gmail.com>
2019-08-19 11:30:09 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from PIL import Image, ImageDraw, ImageOps
 import random
+import pickle
+
+IMG_SIZE = 512
+
+def save_to_file(file_name, obj):
+    with open(file_name, 'wb') as f:
+        pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_from_file(file_name):
+    with open(file_name, 'rb') as f:
+        return pickle.load(f)
 
 def get_edges():
     edges = []
-    for i in range(16, 512, 16):
+    for i in range(16, IMG_SIZE, 16):
         edges.append((0, i))
-        edges.append((512, i))
-        edges.append((i, 512))
+        edges.append((IMG_SIZE, i))
+        edges.append((i, IMG_SIZE))
         edges.append((i, 0))
     return edges
 
@@ -22,43 +33,49 @@ def fitness_function(imt, img):
 
 def open_original_image(file_name):
     im = Image.open(file_name)
-    im = ImageOps.fit(image=im, size=(512, 512), method=0, bleed=0.0, centering=(0.5, 0.5))
+    im = ImageOps.fit(image=im, size=(IMG_SIZE, IMG_SIZE), method=0, bleed=0.0, centering=(0.5, 0.5))
     im = ImageOps.grayscale(image=im)
     im.save(file_name+'_original.png')
     return im
 
-def draw_genome(genome):
-    im = Image.new(mode='RGB', size=(512, 512), color=(255, 255, 255))
+def draw_genome(edges, genome):
+    im = Image.new(mode='RGB', size=(IMG_SIZE, IMG_SIZE), color=(255, 255, 255))
     im = ImageOps.grayscale(image=im)
     d = ImageDraw.Draw(im)
     d.line(xy=[edges[g] for g in genome], fill=0, width=1)
     return im
 
-edges = get_edges()
-genome = [random.randint(0, len(edges)-1) for i in range(1000)]
+def process(n_steps, edges, genome):
+    imt = open_original_image(file_name="1.jpg")
+    img = draw_genome(edges, genome)
+    min_fitness = fitness_function(imt, img)
+    print('min_fitness =', min_fitness)
 
-imt = open_original_image(file_name="1.jpg")
-img = draw_genome(genome)
-min_fitness = fitness_function(imt, img)
-print(-1, '=', min_fitness)
+    cnt = 0
+    for i in range(n_steps):
+        new_genome = list(genome)
+        new_genome[random.randint(0, len(genome)-1)] = random.randint(0, len(edges)-1)
+        img = draw_genome(edges, new_genome)
 
-cnt = 0
-for i in range(500000):
-    new_genome = list(genome)
-    new_genome[random.randint(0, len(genome)-1)] = random.randint(0, len(edges)-1)
-    img = draw_genome(new_genome)
+        fitness = fitness_function(imt, img)
+        if fitness < min_fitness:
+            min_fitness = fitness
+            genome = new_genome
+            print(i, '=', fitness)
+            if cnt % 10 == 0:
+                img.save("gen/"+str(i)+'_'+str(fitness)+'.png')
+                save_to_file("gen/"+str(i)+'_'+str(fitness)+'.gen', genome)
 
-    fitness = fitness_function(imt, img)
-    if fitness < min_fitness:
-        min_fitness = fitness
-        genome = new_genome
-        print(i, '=', fitness)
-        cnt += 1
-        if cnt % 10 == 0:
-            img.save("gen/"+'step_'+str(i)+'_fit_'+str(fitness)+'.png')
+    img.save("gen/"+str(n_steps)+'_'+str(fitness)+'.png')
+    save_to_file("gen/"+str(n_steps)+'_'+str(fitness)+'.gen', genome)
 
-img = draw_genome(genome)
-img.save('result.png')
+def main(genome_file_name):
+    edges = get_edges()
+    if genome_file_name:
+        genome = load_from_file(genome_file_name)
+    else:
+        genome = [random.randint(0, len(edges)-1) for i in range(1000)]
+    process(100000, edges, genome)
 
-
-
+if __name__ == "__main__":
+    main(None)
