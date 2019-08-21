baa3d7e62af0973aa946d952877e1c4aae8b88f7
Alexey Kuklin <AlexeyKuklin@gmail.com>
2019-08-21 21:56:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from PIL import Image, ImageDraw, ImageOps
 import random
 import pickle
 
-IMG_SIZE = 512
-
 def save_to_file(file_name, obj):
     with open(file_name, 'wb') as f:
         pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
@@ -12,33 +10,17 @@ def load_from_file(file_name):
     with open(file_name, 'rb') as f:
         return pickle.load(f)
 
-def get_edges():
-    edges = []
-    for i in range(16, IMG_SIZE, 16):
-        edges.append((0, i))
-        edges.append((IMG_SIZE, i))
-        edges.append((i, IMG_SIZE))
-        edges.append((i, 0))
-    return edges
-
 def fitness_function(pt, pg, pm):
     fitness = 0
-    #fitness = fitness + abs(t[0] - g[0]) + abs(t[1] - g[1]) + abs(t[2] - g[2])
     if pm:
         for t, g, m in zip(pt, pg, pm):
             fitness = fitness + m*abs(t - g)
     else:
         for t, g in zip(pt, pg):
             fitness = fitness + abs(t - g)
+            #fitness = fitness + abs(t[0] - g[0]) + abs(t[1] - g[1]) + abs(t[2] - g[2])
 
     return fitness
-
-def open_original_image(file_name):
-    im = Image.open(file_name)
-    im = ImageOps.fit(image=im, size=(IMG_SIZE, IMG_SIZE), method=0, bleed=0.0, centering=(0.5, 0.5))
-    im = ImageOps.grayscale(image=im)
-    im.save(file_name+'_original.png')
-    return im
 
 def open_mask(file_name):
     im = Image.open(file_name)
@@ -51,11 +33,13 @@ def open_mask(file_name):
     im.save(file_name+'_mask.png')
     return im
 
-def draw_genome(edges, genome):
-    im = Image.new(mode='RGB', size=(IMG_SIZE, IMG_SIZE), color=(255, 255, 255))
-    im = ImageOps.grayscale(image=im)
+def draw_genome(edges, genome, size):
+    im = Image.new(mode='RGB', size=size, color=(255, 255, 255))
     d = ImageDraw.Draw(im)
-    d.line(xy=[edges[g] for g in genome], fill=0, width=1)
+    for i in range(len(genome)-2):
+        d.line(xy=(edges[genome[i][0]], edges[genome[i+1][0]]), fill=(0, 0, 0), width=1)
+        #d.line(xy=(edges[genome[i][1]], edges[genome[i+1][1]]), fill=(255, 255, 255, 50), width=1)
+    im = ImageOps.grayscale(image=im) ###
     return im
 
 def process(n_steps, edges, genome, imt, mask):
@@ -64,33 +48,37 @@ def process(n_steps, edges, genome, imt, mask):
     if mask:
         pm = list(mask.getdata())
 
-    img = draw_genome(edges, genome)
+    img = draw_genome(edges, genome, imt.size)
     pg = list(img.getdata())
     min_fitness = fitness_function(pt, pg, pm)
     print("min_fitness =", min_fitness)
         
     for i in range(n_steps):
-        new_genome = list(genome)
-        new_genome[random.randint(0, len(genome)-1)] = random.randint(0, len(edges)-1)
-        img = draw_genome(edges, new_genome)
+        n = random.randint(0, len(genome)-1)
+        ev_old = genome[n][0]
+        genome[n][0] = random.randint(0, len(edges)-1)
+
+        img = draw_genome(edges, genome, imt.size)
         pg = list(img.getdata())
 
         fitness = fitness_function(pt, pg, pm)
         if fitness < min_fitness:
             min_fitness = fitness
-            genome = new_genome
             print(i, '=', fitness)
             img.save("gen/"+str(i)+'_'+str(fitness)+'.png')
             save_to_file("gen/"+str(i)+'_'+str(fitness)+'.gen', genome)
+        else:
+            genome[n][0] = ev_old
 
-
-def main(n_steps, original_file_name, genome_file_name=None, genome_size=None, mask_file_name=None):
-    imt = open_original_image(file_name=original_file_name)
+def main(imt, edges, n_steps, genome_file_name=None, genome_size=None, mask_file_name=None):
     if genome_file_name:
         genome = load_from_file(genome_file_name)
     else:
-        if genome_size > 0:
-            genome = [random.randint(0, len(edges)-1) for i in range(genome_size)]      
+        if genome_size and genome_size > 0:
+            genome = [[random.randint(0, len(edges)-1), 
+                       random.randint(0, len(edges)-1)
+                     ] for i in range(genome_size)]      
+            #genome = [[0, 0] for i in range(genome_size)]      
         else:
             raise ValueError('genome size is not defined')
 
@@ -98,9 +86,27 @@ def main(n_steps, original_file_name, genome_file_name=None, genome_size=None, m
     if mask_file_name:
         mask = open_mask(file_name=mask_file_name)
 
-    edges = get_edges()
     process(n_steps, edges, genome, imt, mask)
 
 if __name__ == "__main__":
-    main(1000000, 'Girl with a Pearl Earring.jpg')
+    file_name = 'Girl with a Pearl Earring.jpg'
+    im = Image.open(file_name)
+    im = ImageOps.grayscale(image=im)
+    rgbimg = Image.new("RGB", im.size)
+    rgbimg.paste(im)
+    im = rgbimg
+    area = (700, 500, 700+3100, 500+3100)
+    size = (512, 512)
 
+    im = ImageOps.grayscale(image=im) ###
+    im = im.crop(area).resize(size, Image.LANCZOS)
+    im.save(file_name+'_original.png')
+
+    edges = []
+    for i in range(16, 512, 16):
+        edges.append((0, i))
+        edges.append((512, i))
+        edges.append((i, 512))
+        edges.append((i, 0))
+ 
+    main(im, edges, 2000000, genome_size=1000)
